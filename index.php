11fa2c4466ad7fa7b8934c7178bf80f5b735d9a8
unknown <ahmedghnam116@gmail.com>
2025-02-15 01:24:06 +0200

    navbar test design

--- index.php
+++ index.php
@@ -9,8 +9,15 @@
 
     <?php include('templates/header.php'); ?>
     
-    
-    
+    <div class="">
+        <ul class="adel">
+            <div class="row">
+                <li class="col-sm-1">بقالة بالوزن</li>
+                <li class="col-sm-1">بقالة جافة</li>
+                <li class="col-sm-1">مجمدات</li>
+            </div>
+        </ul>
+    </div>
     
     
     
