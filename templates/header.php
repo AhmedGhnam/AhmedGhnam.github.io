8a27fede1e81ae99c80fe19121113e392f42a6c6
unknown <ahmedghnam116@gmail.com>
2025-02-16 22:49:02 +0200

    background modification

--- templates/header.php
+++ templates/header.php
@@ -7,21 +7,26 @@
     <title>Document</title>
 </head>
 <body class="">
+<?php 
+
+    if(!isset($noNavbar)) {
+?>        
 <nav class="navbar navbar-star navbar-expand-lg bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="#"><img src="images/star.png" alt=""></a> 
+        <a class="navbar-brand" href="index.php"><img src="images/star.png" alt=""></a> 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">الصفحة الرئيسية</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">الصفحة الرئيسية</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">العاملين</a>
+                    <a class="nav-link" href="employs.php">العاملين</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+<?php } ?>
